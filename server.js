b16a1897babcf704974d9ef52aadca5978086f23
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-09-03 17:06:12 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const jsonServer = require('json-server')
+const auth = require('json-server-auth')
+
+const server = jsonServer.create()
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+const router = jsonServer.router('./data/db.json')
+server.db = router.db
+
+const middlewares = jsonServer.defaults()
+const rules = auth.rewriter({
+    products: 444,
+    featured_products: 444,
+    orders: 660,
+    users: 600
+  })
+
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
+
+server.listen(8000, () => {
+  console.log('JSON Server is running')
+})
