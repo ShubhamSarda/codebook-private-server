cac6cabd213d9f7de13c914a3e66b3fb7363ae64
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-09-03 17:41:28 +0530

    Changes 4

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+const express = require('express');
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
 
-const server = jsonServer.create()
+const server = express();
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', '*')
     next()
 })
 
-const router = jsonServer.router('./data/db.json')
+const router = jsonServer.router('./data/db.json');
+server.use('/', router);
 server.db = router.db
 
 const middlewares = jsonServer.defaults()
@@ -24,6 +26,4 @@ server.use(auth)
 server.use(middlewares)
 server.use(router)
 
-server.listen(8000, () => {
-  console.log('JSON Server is running')
-})
+server.listen(8000);
